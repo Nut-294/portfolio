00ed70c476c931ad713356450fdfbad1731e6a7b
Nut-294 <134218655+Nut-294@users.noreply.github.com>
2025-07-10 12:15:05 +0700

    Navbar

---
+++ components/Navbar/NavBar.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { Menubar, MenubarMenu, MenubarTrigger } from "@/components/ui/menubar";
+
+export default function Navbar() {
+  return (
+    <Menubar className="fixed top-0 z-50 w-full justify-between px-6 py-6 bg-white/80 backdrop-blur-md shadow-md">
+      <div className="flex gap-6">
+        <MenubarMenu>
+          <MenubarTrigger>
+            <a href="#home">Home</a>
+          </MenubarTrigger>
+        </MenubarMenu>
+        <MenubarMenu>
+          <MenubarTrigger>
+            <a href="#about">About</a>
+          </MenubarTrigger>
+        </MenubarMenu>
+        <MenubarMenu>
+          <MenubarTrigger>
+            <a href="#projects">Projects</a>
+          </MenubarTrigger>
+        </MenubarMenu>
+        <MenubarMenu>
+          <MenubarTrigger>
+            <a href="#contact">Contact</a>
+          </MenubarTrigger>
+        </MenubarMenu>
+      </div>
+
+      
+      <div className="hidden md:flex items-center gap-4">
+        <a
+          href="/resume.pdf"
+          className="text-sm bg-black text-white px-4 py-2 rounded-md hover:opacity-80 transition"
+        >
+          Download Resume
+        </a>
+      </div>
+    </Menubar>
+  );
+}
